5f7573e674c6e2edbf451eb966a56a4434ee02e8
RupendraCHA <chandaluri210@gmail.com>
2025-03-13 10:43:09 +0530

    Calculation of BMI

--- BMICalculation/script.js
+++ BMICalculation/script.js
@@ -1,4 +1,5 @@
 let bmiForm = document.querySelector(".bmi-form")
+let resetBtn = document.querySelector("#reset")
 
 bmiForm.addEventListener("submit", function (e) {
     e.preventDefault()
@@ -6,8 +7,31 @@ bmiForm.addEventListener("submit", function (e) {
     const form = e.target
 
     const name = form.querySelector("#name").value
-    const height = form.querySelector("#height").value
-    const weight = form.querySelector("#weight").value
+    const weight = +form.querySelector("#weight").value
+    const height = +form.querySelector("#height").value
 
-    
+    const result = form.querySelector("#result")
+    console.log(result)
+
+    console.log(height)
+    console.log((height/100)**2)
+    console.log(weight)
+
+    const heightToWeightToRatio = (weight / ((height/100) ** 2))
+
+    if (heightToWeightToRatio <= 18){
+        result.textContent = `${name.toUpperCase()}, your BMI is ${heightToWeightToRatio.toFixed()} and you are Thin`
+    }else if (heightToWeightToRatio > 18 && heightToWeightToRatio <= 25){
+        result.textContent = `${name.toUpperCase()}, your BMI is ${heightToWeightToRatio.toFixed()} and you are Healthy`
+
+    }else{
+        result.textContent = `${name.toUpperCase()}, your BMI is ${heightToWeightToRatio.toFixed()} and you are Fat`
+    }
+})
+
+resetBtn.addEventListener("click", function (e) {
+    document.querySelector("#name").value = ""
+    document.querySelector("#result").textContent = ''
+    document.querySelector("#weight").value = ''
+    document.querySelector("#height").value = ''
 })
